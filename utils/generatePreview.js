882489151ce2a2aae95d060d890c33f8204d6a0a
yannis <yannis@behaviourlab.com>
2021-07-17 09:21:48 +0100

    feat(event-shapes): created dynamic short vertical and horizontal bar shapes

--- utils/generatePreview.js
+++ utils/generatePreview.js
@@ -3,7 +3,7 @@ function generatePreview(shape, dimensions, x, y) {
   const dict = {}
   switch(shape) {
     case "horizontal_bar":
-      for(var i=0; i<height; ++i){
+      for(var i=0; i<width; ++i){
         if (!dict[x]) {
           dict[x] = { [i]: true }
         } else{
@@ -12,7 +12,7 @@ function generatePreview(shape, dimensions, x, y) {
       }
       break;
     case "vertical_bar":
-      for (var i=0; i<width; ++i){
+      for (var i=0; i<height; ++i){
         if (!dict[i]) {
           dict[i] = { [y]: true }
         } else{
@@ -20,6 +20,27 @@ function generatePreview(shape, dimensions, x, y) {
         }
       }
       break;
+    case "short_horizontal_bar":
+      var upperLimit = Math.min(y + Math.floor(width / 2), width)
+      for (var i=y; i<upperLimit; ++i){
+        if (!dict[x]) {
+          dict[x] = { [i]: true }
+        } else{
+          dict[x][i] = true
+        }
+      }
+      break;
+    case "short_vertical_bar":
+      var upperLimit = Math.min(x + Math.floor(height / 2), height)
+      for (var i=x; i<upperLimit; ++i){
+        if (!dict[i]) {
+          dict[i] = { [y]: true }
+        } else{
+          dict[i][y] = true
+        }
+      }
+      break;
+
     default:
       // shouldn't happen
       console.error("Received unknown shape")
--- utils/updateCaptures.js
+++ utils/updateCaptures.js
@@ -19,6 +19,27 @@ function updateCaptures(dict, id, shape, dimensions, x, y) {
         }
       }
       break
+      case "short_horizontal_bar":
+        var upperLimit = Math.min(y + Math.floor(width / 2), width)
+        for (var i=y; i<upperLimit; ++i){
+          if (!dict[x]) {
+            dict[x] = { [i]: id }
+          } else{
+            dict[x][i] = id
+          }
+        }
+        break;
+  
+      case "short_vertical_bar":
+        var upperLimit = Math.min(x + Math.floor(height / 2), height)
+        for (var i=x; i<upperLimit; ++i){
+          if (!dict[i]) {
+            dict[i] = { [y]: id }
+          } else{
+            dict[i][y] = id
+          }
+        }
+        break;
     default:
       // shouldn't happen
       console.error("Received unknown shape")
