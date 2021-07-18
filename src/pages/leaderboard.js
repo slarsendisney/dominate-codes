fffd6f3b7c2b4bb2027e182339f5f9954bf25d4f
yannis <yannis@behaviourlab.com>
2021-07-18 08:48:50 +0100

    feat(leaderboard): added base leaderboard logic

--- src/pages/leaderboard.js
+++ src/pages/leaderboard.js
@@ -3,10 +3,11 @@ import * as React from "react"
 import bronzeTrophy from "../assets/bronzeTrophy.svg"
 import silverTrophy from "../assets/silverTrophy.svg"
 import goldTrophy from "../assets/goldTrophy.svg"
-const Leaderboard = () => {
+const Leaderboard = ({ playerRanking }) => {
 
-  // const top3 = ranking.slice(0, 2)
-  const remainder = ["Sam", "Giles", "Francis", "Larsen", "Disney"]
+  const topPlayers = playerRanking.slice(0, 2)
+  const otherPlayers = playerRanking.slice(3)
+
   return (
     <div className="max-w-7xl mx-auto mt-8">
       {/* Top 3 */}
@@ -14,21 +15,24 @@ const Leaderboard = () => {
         <div className="order-3 md:order-1 bg-white shadow rounded-md border-2 border-indigo-600 w-full h-full p-5 text-center">
           <img src={bronzeTrophy} className="w-5/12 mx-auto" />
           <p className="font-bold text-xl my-2">3rd</p>
+          <p>{topPlayers[2]}</p>
         </div>
         <div className="order-1 md:order-2 bg-white shadow rounded-md border-2 border-indigo-600 w-full h-full p-5 text-center">
           <img src={goldTrophy} className="w-5/12 mx-auto" />
           <p className="font-bold text-xl my-2">1st</p>
+          <p>{topPlayers[0]}</p>
         </div>
         <div className="order-2 md:order-3 bg-white shadow rounded-md border-2 border-indigo-600 w-full h-full p-5 text-center">
           <img src={silverTrophy} className="w-5/12 mx-auto"/>
           <p className="font-bold text-xl my-2">2nd</p>
+          <p>{topPlayers[1]}</p>
         </div>
       </div>
 
       {/* remainder */}
-      {remainder.map((user, i) => (
+      {otherPlayers.map((player, i) => (
         <div className="w-full bg-white shadow rounded-md border-2 border-indigo-600 p-5 my-4">
-          <p>{`${i+4}. ${user}`}</p>
+          <p>{`${i+4}. ${player}`}</p>
         </div>
       ))}
     </div>
